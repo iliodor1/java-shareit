06fa4e70e56857ace8d3fefdd9e365322008e972
Eldar <eldar.gai@yandex.ru>
2022-07-24 14:00:42 +0500

    feat: add ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * // TODO .
- */
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class ItemDto {
+    Long id;
+    @NotBlank
+    String name;
+    @NotBlank
+    String description;
+    @NotNull
+    Boolean available;
+
 }
