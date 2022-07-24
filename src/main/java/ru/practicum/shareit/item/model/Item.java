46200713b3f014d6161cfec84c4e19ea3f883294
Eldar <eldar.gai@yandex.ru>
2022-07-24 14:00:01 +0500

    feat: add Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,19 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * // TODO .
- */
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class Item {
+
+    Long id;
+    String name;
+    String description;
+    Boolean available;
+    Long userId;
+
 }
