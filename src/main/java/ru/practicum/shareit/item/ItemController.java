742d5440df02d3d5875ad57b39a8af33ee565aa5
Eldar <eldar.gai@yandex.ru>
2022-07-24 14:00:27 +0500

    feat: add ItemController

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,12 +1,47 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.ItemDto;
 
-/**
- * // TODO .
- */
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
 @RestController
 @RequestMapping("/items")
+@RequiredArgsConstructor
 public class ItemController {
+    private final ItemService itemService;
+
+    @GetMapping("{id}")
+    public ItemDto getItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                           @PathVariable Long id) {
+        return itemService.getItem(userId, id);
+    }
+
+    @GetMapping
+    public List<ItemDto> getOwnItems(@RequestHeader("X-Sharer-User-Id") Long userId) {
+        return itemService.getOwnItems(userId);
+    }
+
+    @PostMapping
+    public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                           @Valid @RequestBody ItemDto itemDto) {
+        return itemService.addItem(userId, itemDto);
+    }
+
+    @PatchMapping("{itemId}")
+    public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                              @PathVariable Long itemId,
+                              @RequestBody ItemDto itemDto) {
+        return itemService.update(userId, itemId, itemDto);
+    }
+
+    @GetMapping("search")
+    public List<ItemDto> searchItem(@RequestHeader("X-Sharer-User-Id") Long userId,
+                                    @Valid @NotBlank @RequestParam String text){
+        return itemService.searchItem(userId, text);
+    }
+
 }
