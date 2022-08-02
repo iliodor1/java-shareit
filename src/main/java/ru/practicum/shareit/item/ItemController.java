5896a21433fa97915790bea0f483464abc2bb132
Eldar <eldar.gai@yandex.ru>
2022-08-02 09:14:01 +0500

    feat: add validation in update method

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -1,47 +1,49 @@
 package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.validator.Marker.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @RestController
-@RequestMapping("/items")
+@RequestMapping("items")
 @RequiredArgsConstructor
+@Validated
 public class ItemController {
     private final ItemService itemService;
 
     @GetMapping("{id}")
-    public ItemDto getItem(@RequestHeader("X-Sharer-User-Id") Long userId,
-                           @PathVariable Long id) {
-        return itemService.getItem(userId, id);
+    public ItemDto getItem(@PathVariable Long id) {
+        return itemService.getItem(id);
     }
 
     @GetMapping
-    public List<ItemDto> getOwnItems(@RequestHeader("X-Sharer-User-Id") Long userId) {
-        return itemService.getOwnItems(userId);
+    public List<ItemDto> getOwnItems(@RequestHeader("X-Sharer-User-Id") Long ownerId) {
+        return itemService.getOwnItems(ownerId);
     }
 
     @PostMapping
-    public ItemDto addItem(@RequestHeader("X-Sharer-User-Id") Long userId,
-                           @Valid @RequestBody ItemDto itemDto) {
-        return itemService.addItem(userId, itemDto);
+    @Validated(OnCreate.class)
+    public ItemDto create(@RequestHeader("X-Sharer-User-Id") Long ownerId,
+                          @Valid @RequestBody ItemDto itemDto) {
+        return itemService.create(ownerId, itemDto);
     }
 
-    @PatchMapping("{itemId}")
-    public ItemDto updateItem(@RequestHeader("X-Sharer-User-Id") Long userId,
-                              @PathVariable Long itemId,
-                              @RequestBody ItemDto itemDto) {
-        return itemService.update(userId, itemId, itemDto);
+    @PatchMapping("{id}")
+    @Validated(OnUpdate.class)
+    public ItemDto update(@RequestHeader("X-Sharer-User-Id") Long ownerId,
+                          @PathVariable Long id,
+                          @Valid @RequestBody ItemDto itemDto) {
+        return itemService.update(ownerId, id, itemDto);
     }
 
     @GetMapping("search")
-    public List<ItemDto> searchItem(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                    @Valid @NotBlank @RequestParam String text) {
-        return itemService.searchItem(userId, text);
+    public List<ItemDto> searchItem(@RequestParam String text) {
+        return itemService.searchItem(text);
     }
 
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,21 +1,25 @@
 package ru.practicum.shareit.user;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.validator.Marker.*;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/users")
+@RequestMapping(path = "users")
 @RequiredArgsConstructor
+@Validated
 public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public UserDto addUser(@Valid @RequestBody UserDto userDto) {
-        return userService.addUser(userDto);
+    @Validated({OnCreate.class})
+    public UserDto create(@Valid @RequestBody UserDto userDto) {
+        return userService.create(userDto);
     }
 
     @GetMapping("{id}")
@@ -24,13 +28,14 @@ public class UserController {
     }
 
     @PatchMapping("{id}")
-    public UserDto updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
-        return userService.updateUser(id, userDto);
+    @Validated({OnUpdate.class})
+    public UserDto update(@PathVariable Long id, @Valid @RequestBody UserDto userDto) {
+        return userService.update(id, userDto);
     }
 
     @DeleteMapping("{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userService.deleteUser(id);
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
     }
 
     @GetMapping
