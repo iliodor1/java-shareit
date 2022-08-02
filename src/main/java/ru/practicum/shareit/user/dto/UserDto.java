a656d6217d8e532d53acaa0dfe5a44c66ecf6cb8
Eldar <eldar.gai@yandex.ru>
2022-08-02 09:17:45 +0500

    feat: add validation in update method

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,18 +2,21 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.validator.Marker.*;
+import ru.practicum.shareit.validator.NullOrNotBlank;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Data
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class UserDto {
     Long id;
-    @NotBlank
+    @NotBlank(groups = OnCreate.class, message = "The name should not be null or blank.")
+    @NullOrNotBlank(groups = OnUpdate.class, message = "The name should be null or not blank.")
     String name;
     @Email
-    @NotBlank
+    @NotBlank(groups = OnCreate.class, message = "The email should not be null or blank.")
+    @NullOrNotBlank(groups = OnUpdate.class, message = "The email should be null or not blank.")
     String email;
 
 }
