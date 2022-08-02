e9ff3496544a7807ee711d9620314afe47907bdf
Eldar <eldar.gai@yandex.ru>
2022-08-02 09:14:30 +0500

    feat: add fields

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item;
 import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.ItemRequest;
 
 @Component
 public class ItemMapper {
@@ -12,17 +13,19 @@ public class ItemMapper {
         String name = item.getName();
         String description = item.getDescription();
         Boolean available = item.getAvailable();
+        ItemRequest request = item.getItemRequest();
 
-        return new ItemDto(id, name, description, available);
+        return new ItemDto(id, name, description, available, request);
     }
 
-    public Item toItem(ItemDto itemDto, Long userId) {
+    public Item toItem(ItemDto itemDto, Long ownerId) {
         Long id = itemDto.getId();
         String name = itemDto.getName();
         String description = itemDto.getDescription();
         Boolean available = itemDto.getAvailable();
+        ItemRequest request = itemDto.getItemRequest();
 
-        return new Item(id, name, description, available, userId);
+        return new Item(id, name, description, available, ownerId, request);
     }
 
 }
