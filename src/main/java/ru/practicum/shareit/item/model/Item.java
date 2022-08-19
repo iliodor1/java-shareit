3893d0828b3a80e12f465d43e455d70cd442f5ba
Eldar <eldar.gai@yandex.ru>
2022-08-19 19:03:14 +0500

    feat: add persistence annotations

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,20 +1,48 @@
 package ru.practicum.shareit.item.model;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.booking.Booking;
+import ru.practicum.shareit.item.Comment;
 import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.mopel.User;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static javax.persistence.GenerationType.IDENTITY;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "items")
 public class Item {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     Long id;
     String name;
     String description;
     Boolean available;
-    Long ownerId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", referencedColumnName = "id")
+    User owner;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_request_id", referencedColumnName = "id")
     ItemRequest itemRequest;
+    @OneToMany
+    List<Booking> bookings;
+    @OneToMany
+    List<Comment> comments;
+
+    public Item(Long id, String name, String description, Boolean available, User owner) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.owner = owner;
+    }
 
 }
