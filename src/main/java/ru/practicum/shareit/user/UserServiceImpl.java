32c52e6567ea21c89ba8ca3edc9454059788562b
Eldar <eldar.gai@yandex.ru>
2022-08-19 19:06:06 +0500

    feat: exchange  UserRepository to UserJpaRepository

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -5,52 +5,62 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exeption.NotFoundException;
 import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.mopel.User;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
+    private final UserJpaRepository userRepository;
     private final UserMapper userMapper;
 
     @Override
     public UserDto create(UserDto userDto) {
         User user = userMapper.toUser(userDto);
 
-        return userMapper.toDto(userRepository.create(user));
+        return userMapper.toDto(userRepository.save(user));
     }
 
     @Override
     public UserDto update(Long id, UserDto userDto) {
-        User user = userMapper.toUser(userDto);
-        user.setId(id);
+        User newUser = userMapper.toUser(userDto);
+        User oldUser = userMapper.toUser(getUser(id));
 
-        return userMapper.toDto(userRepository.update(user));
+        if (newUser.getEmail() != null) {
+            oldUser.setEmail(newUser.getEmail());
+        }
+        if (newUser.getName() != null) {
+            oldUser.setName(newUser.getName());
+        }
+
+        return userMapper.toDto(userRepository.save(oldUser));
     }
 
     @Override
     public UserDto getUser(Long id) {
-        User user = userRepository.getUser(id)
+        User user = userRepository.findById(id)
                                   .orElseThrow(() -> {
                                       log.error("User with id " + id + " not found!");
-                                      return new NotFoundException("User with id " + id + " not found!");
+                                      return new NotFoundException(
+                                              "User with id " + id + " not found!"
+                                      );
                                   });
         return userMapper.toDto(user);
     }
 
     @Override
     public void delete(Long id) {
-        userRepository.delete(id);
+        User user = userMapper.toUser(getUser(id));
+        userRepository.delete(user);
     }
 
     @Override
     public List<UserDto> getAll() {
-        List<User> users = userRepository.getAll();
+        List<User> users = userRepository.findAll();
 
         return users.stream()
                     .map(userMapper::toDto)
