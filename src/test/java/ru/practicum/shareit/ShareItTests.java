e00301d2549b252310f5036460c4da571c959c20
Eldar <eldar.gai@yandex.ru>
2022-08-20 15:16:30 +0500

    refactor

--- src/test/java/ru/practicum/shareit/ShareItTests.java
+++ src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ShareItTests.class)
+@SpringBootTest
 class ShareItTests {
 
 	@Test
