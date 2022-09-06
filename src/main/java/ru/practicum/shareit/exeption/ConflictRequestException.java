eb9ff1e194fc181d1fdbc49d6c4e3e957a107fd3
Eldar <eldar.gai@yandex.ru>
2022-09-06 15:00:23 +0500

    add lombok config

--- src/main/java/ru/practicum/shareit/exeption/ConflictRequestException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exeption;
-
-public class ConflictRequestException extends RuntimeException {
-    public ConflictRequestException(String message) {
-        super(message);
-    }
-
-}
